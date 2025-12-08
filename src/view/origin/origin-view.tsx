4ef5a30433d983b2e75f759b42e215985984c764
JangHwanPark <wkd5878@gmail.com>
2025-12-08 15:26:04 +0900

    feat: TechView 컴포넌트 추가 및 Home 페이지 적용
    - Radix Tabs를 활용하여 기술 상세 정보를 보여주는 TechView 컴포넌트 구현
    - Home 페이지에서 OriginView와 TechView를 함께 렌더링하도록 수정
    - 네이밍 일관성을 위해 Origin 컴포넌트를 OriginView로 변경
    - @radix-ui/react-tabs 의존성 추가
    - src/view에서 TechView export 추가

--- src/view/origin/origin-view.tsx
+++ src/view/origin/origin-view.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 
-/*interface OriginProps {
-
-};*/
-
-export const Origin = () => {
+export const OriginView = () => {
   return (
     <section id="origin-section" aria-labelledby="origin-heading">
       <div>
