b833a70865e131396db156c3bc042499bf4ef32b
japark <67992955+JangHwanPark@users.noreply.github.com>
2025-12-11 17:32:11 +0900

    Feat: Footer 컴포넌트 추가 및 레이아웃 간소화 (#5)
    * feat: Footer 컴포넌트 추가 및 레이아웃 통합
    
    - Footer 컴포넌트 생성 및 글로벌 레이아웃에 통합.
    - 불필요한 이미지 및 요소 제거로 Home 페이지 간소화.
    - GlobalNavbar 컴포넌트 및 Footer 컴포넌트 exports 추가.
    
    * feat: Footer 컴포넌트 추가 및 레이아웃 통합
    
    - Footer 컴포넌트 생성 및 글로벌 레이아웃에 통합.
    - 불필요한 이미지 및 요소 제거로 Home 페이지 간소화.
    - GlobalNavbar 컴포넌트 및 Footer 컴포넌트 exports 추가.

---
+++ src/widgets/layout/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer = () => {
+  return (
+    <footer className="w-full py-5">
+      <p className="text-center">
+        © 2025 <span className="font-bold">JangHwanPark.</span> All Rights Reserved.
+      </p>
+    </footer>
+  );
+};
---
+++ src/widgets/layout/index.ts
@@ -0,0 +1,3 @@
+export { Header } from './header';
+export { GlobalNavbar } from './global-navbar';
+export { Footer } from './footer';
