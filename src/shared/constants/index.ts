ccd8c01a0ad86c421c49024c5ed8dd9484177ef7
japark <67992955+JangHwanPark@users.noreply.github.com>
2025-12-11 21:18:23 +0900

    Integrate ProjectCard component and improve PlaygroundView layout (#7)
    * feat: PlaygroundView 스타일 업데이트 및 레이아웃 개선
    
    - 섹션에 flex 레이아웃 및 전체 화면 높이 적용.
    - 헤더와 리스트에 스타일 추가로 UI 가독성 향상.
    - ul 요소를 ol 요소로 변경하여 의미론적 구조 개선.
    
    * feat: 프로젝트 관련 엔티티 및 컴포넌트 추가
    
    - ProjectSchema 생성 및 프로젝트 데이터 스키마 정의.
    - 기술 스택 및 역할 상수를 포함한 constants 추가.
    - ProjectCard 컴포넌트 기본 구조 생성 및 export 설정.
    - 프로젝트와 관련된 엔티티 및 상수 shared 디렉토리에 분리.
    
    * feat: 프로젝트 엔티티 및 상수 확장, ProjectSchema 수정
    
    - 프로젝트 역할 및 상태 상수(PROJECT_ROLE, PROJECT_STATUS) 추가.
    - 프로젝트 관련 데이터(ProjectRole, ProjectStatus) 분리 및 constants에 정의.
    - 새로운 프로젝트 데이터 프로젝트(entity/project.ts)로 추가.
    - ProjectSchema 수정: 역할, 책임, 기능, 과제 필드 추가 및 기본값 설정.
    - 기술 스택 상수 및 관련 타입 정리.
    
    * feat: ProjectCard 컴포넌트 통합 및 PlaygroundView 레이아웃 개선
    
    - ProjectCard 컴포넌트 완성 및 스타일 추가.
    - PlaygroundView에서 ol 요소를 div로 변경하고 ProjectCard 컴포넌트 적용.
    - ProjectCard export 설정 추가 및 features 디렉토리에 등록.

---
+++ src/shared/constants/index.ts
@@ -0,0 +1,2 @@
+export { TECH_STACKS, type TechStack } from './tech';
+export { PROJECT_ROLE, type ProjectRole } from './projects';
--- src/view/playground/playground-view.tsx
+++ src/view/playground/playground-view.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
+import {ProjectCard} from "@/features";
 
 export const PlaygroundView = () => {
   return (
-    <section id="playground-section">
+    <section id="playground-section" className="flex h-[100vh] flex-col items-start justify-start">
       <header>
-        <h2>Playground</h2>
+        <h2 className="mx-auto mt-32 mb-5 w-full max-w-3xl bg-gradient-to-b from-[#F4B400] to-white bg-clip-text px-4 text-4xl font-bold text-transparent md:mt-10 md:mb-16 md:max-w-6xl lg:ml-20 lg:text-6xl 2xl:text-7xl">
+          Playground
+        </h2>
       </header>
-      <ul>
-        <li>Project 01</li>
-        <li>Project 02</li>
-        <li>Project 03</li>
-        <li>...</li>
-      </ul>
+      <div className="relative mb-0 flex w-full gap-10 before:absolute before:top-0 before:h-[2px] before:w-[50000vw] before:bg-white before:content-[''] after:absolute after:top-[100%] after:h-[2px] after:w-[50000vw] after:bg-white after:content-[''] lg:h-[80%]">
+        <ProjectCard/>
+        <ProjectCard/>
+        <ProjectCard/>
+        <ProjectCard/>
+        <ProjectCard/>
+      </div>
     </section>
   );
 };
