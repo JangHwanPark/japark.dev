2babac2cc2ffe3f1cfbca6ab1045138cd5455202
JangHwanPark <wkd5878@gmail.com>
2025-12-12 09:32:56 +0900

    feat: Home 및 OriginView 레이아웃 조정 및 Line 컴포넌트 통합
    - Home 페이지에 Line 컴포넌트 추가 및 레이아웃 개선.
    - OriginView에서 Line 컴포넌트 제거 및 구조 단순화.
    - 코드 가독성 및 스타일링 정리.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import {MilestonesView, OriginView, PlaygroundView, TechView} from '@/view';
+import {Line} from "@/shared";
+import React from "react";
 
 export default function Home() {
   return (
     <main className="grid grid-cols-12 mx-auto max-w-screen-xl gap-x-6 gap-y-16 px-4 sm:px-8">
+      <Line.Linear />
       <OriginView/>
+      <Line.Root className="relative w-full lg:block">
+        <Line.CurveFirst className="h-52 lg:ml-38 lg:w-8/12 xl:ml-112 xl:w-6/12" />
+        <Line.CurveLast className="h-72 w-4/12 lg:ml-38 xl:ml-112" />
+      </Line.Root>
       <TechView/>
       <PlaygroundView/>
       <MilestonesView/>
