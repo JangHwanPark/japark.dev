b9a00eb3219bb58bc1a6be8201398e16d1d4b71f
JangHwanPark <wkd5878@gmail.com>
2025-12-15 15:52:27 +0900

    feat: Add ProjectView component and integrate into Home page
    - Created `ProjectView` component for showcasing featured projects.
    - Added `ProjectView` section in Home page layout.
    - Exported `ProjectView` from `view/project` directory.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import {MilestonesView, OriginView, PlaygroundView, TechView} from '@/view';
 import {Line} from "@/shared";
 import React from "react";
 import {AboutView} from "@/view/about";
+import {ProjectView} from "@/view/project";
 
 export default function Home() {
   return (
     <main className='col-span-12'>
       <AboutView/>
+      <ProjectView/>
       <OriginView/>
       <PlaygroundView/>
       <TechView/>
