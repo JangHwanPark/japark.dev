4357e2038772b589867182ff771437130c5a2959
JangHwanPark <wkd5878@gmail.com>
2025-12-18 13:37:43 +0900

    fix: 애니메이션이 적용되지 않던 문제 픽스

--- src/features/tech-slider/tech-slider.tsx
+++ src/features/tech-slider/tech-slider.tsx
@@ -7,7 +7,7 @@ const SLIDER_ITEM = [...TECH_ALL, ...TECH_ALL];
 export const TechSlider = () => {
   return (
     <div className='col-span-12 overflow-x-hidden flex py-6 fade-mask' aria-hidden={true}>
-      <div className='flex gap-10 justify-center animate-marquee'>
+      <div className='flex gap-10 justify-center [animation:var(--animate-marquee)]'>
         {SLIDER_ITEM.map((item, index) => (
           <div key={`${item.id}-${index}`} className='w-fit flex items-center gap-2 text-5xl font-black'>
             <TechUi id={item.id} className='w-12 h-12'/>
